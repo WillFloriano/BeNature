62b6022716f81e995e4a3a99c979876113f67cbc
Will <william.ffloriano@gmail.com>
2023-02-10 01:35:11 -0300

    favicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,8 @@ function Footer() {
                     <li>©beNature - byNature 2023 Todos os direitos reservados</li>
                     <li>CNPJ: 000.000.000/0000-00</li>
                     <li>Tel: (11)00000-0000</li>
-                </ul>
+                </ul>                
+                    <div className="powered">Powered by WF Tecnology</div>                
             </div>
         </>
     );
